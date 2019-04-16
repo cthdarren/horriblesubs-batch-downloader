eb3b7c590dc0b605926c2b43537e0c726adaa578
Tze Hao Darren Chan <s10179845@connect.np.edu.sg>
2019-04-16 14:39:57 +0800

    added functionality to download all 1080p magnet links, no gui yet

--- horriblesubs.py
+++ horriblesubs.py
@@ -1,14 +1,16 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-import re
-import urllib.request
+import time
 
 nameList = []
 hrefStrings = ""
 linksDict = {}
+quality = ""
 
-startPage = requests.get("https://horriblesubs.info/shows/")
+landingurl = "https://horriblesubs.info/"
+
+startPage = requests.get(landingurl + "shows/")
 
 soup = BeautifulSoup(startPage.text, features="html.parser")
 
@@ -20,21 +22,52 @@ for div in soup.find_all(class_="shows-wrapper"):
 #choose by dropdown, chose name
 chosenAnime = "Sword Art Online – Alicization"
 
-req = urllib.request.Request("https://horriblesubs.info" + linksDict.get(chosenAnime), headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
+url = "https://horriblesubs.info" + linksDict.get(chosenAnime)
 
-chosenPage = urllib.request.urlopen(req)
+# req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
 
-showsoup = BeautifulSoup(chosenPage, features="html.parser")
+# chosenPage = urllib.request.urlopen(req)
 
-print(showsoup.text)
+showsoup = BeautifulSoup(requests.get(url).text, features="html.parser")
 
-quality = "1080p"
-for div in showsoup.find_all(class_="hs-shows"):
-	print(div)
-	for x in div.find_all(class_="link-1080p"):
-		print(x)
-		for y in x.find_all(class_="hs-magnet-link"):
-			print(y)
+scriptList = showsoup.find_all("script")
+
+for tag in scriptList:
+	# print(tag.text)
+	if "hs_showid" in tag.text:
+		showid = tag.text[16:-1]
+
+
+apiLink = "api.php?method=getshows&type=show&showid=" + showid
+
+nextid = 0
+test = 0
+while True:
+	loadedPage = requests.get(landingurl + apiLink + "&nextid=" + str(nextid))
+	if loadedPage.text == 'DONE':
+		print("hello")
+		break
+
+	loadedSoup = BeautifulSoup(loadedPage.text,features="html.parser")
+
+	quality = "1080p"
+
+	for span in loadedSoup.find_all(class_="link-" + quality):
+		for x in span.find_all(class_="hs-magnet-link"):
+			for y in x.find_all("a", href=True):
+				os.startfile(str(y["href"]))
+				print(test)
+				test+=1
+				break
+	nextid += 1
+
+# quality = "1080p"
+# for div in showsoup.find_all(class_="hs-shows"):
+# 	print(div)
+# 	for x in div.find_all(class_="link-1080p"):
+# 		print(x)
+# 		for y in x.find_all(class_="hs-magnet-link"):
+# 			print(y)
 
 
 """
